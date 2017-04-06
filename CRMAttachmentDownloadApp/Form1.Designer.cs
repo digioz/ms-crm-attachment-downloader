44b28aeec05e3845b03c7706e780f1bb7b484839
digioz <digioz@gmail.com>
2017-04-06 13:42:06 -0500

    Migration from Codeplex to Github for version 1.0.0.0 and 2.0.0.0.

---
+++ CRMAttachmentDownloadApp/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CRMAttachmentDownloadApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOrganization = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGUID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gray;
+            this.groupBox1.Controls.Add(this.txtDomain);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtOrganization);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 117);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Information";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(276, 79);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(100, 20);
+            this.txtDomain.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(230, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Domain";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(276, 53);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(276, 26);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Username";
+            // 
+            // txtOrganization
+            // 
+            this.txtOrganization.Location = new System.Drawing.Point(97, 79);
+            this.txtOrganization.Name = "txtOrganization";
+            this.txtOrganization.Size = new System.Drawing.Size(100, 20);
+            this.txtOrganization.TabIndex = 5;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(97, 53);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "80";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(97, 26);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Organizaton";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtGUID
+            // 
+            this.txtGUID.Location = new System.Drawing.Point(12, 188);
+            this.txtGUID.Multiline = true;
+            this.txtGUID.Name = "txtGUID";
+            this.txtGUID.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtGUID.Size = new System.Drawing.Size(409, 155);
+            this.txtGUID.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "GUID (One Per Line)";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDownload.Location = new System.Drawing.Point(286, 406);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(134, 23);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(15, 142);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(324, 20);
+            this.txtPath.TabIndex = 4;
+            this.txtPath.Text = "C:\\";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnBrowse.Location = new System.Drawing.Point(345, 140);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(12, 350);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtError.Size = new System.Drawing.Size(410, 51);
+            this.txtError.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(438, 435);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGUID);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRM Attachment Download Application Version 1.2.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtOrganization;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGUID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtError;
+    }
+}
+
---
+++ CRMAttachmentDownloadApp/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Serialization;
+using System.Reflection;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+using Microsoft.Crm.Sdk;
+using Microsoft.Crm.Sdk.Query;
+using Microsoft.Crm.Sdk.Metadata;
+using Microsoft.Crm.SdkTypeProxy;
+using Microsoft.Crm.SdkTypeProxy.Metadata;
+
+namespace CRMAttachmentDownloadApp
+{
+    public partial class Form1 : Form
+    {
+        #region Initalization Section
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Communication Methods
+        /// <summary>
+        /// Method to create an instance of the 
+        /// Webservice based on the login criteria 
+        /// specified by the user
+        /// </summary>
+        /// <returns></returns>
+        public CrmService GetCrmService()
+        {
+            CrmAuthenticationToken loToken = new CrmAuthenticationToken();
+            loToken.AuthenticationType = 0;
+            loToken.OrganizationName = txtOrganization.Text;
+
+            CrmService loService = new CrmService();
+            loService.CrmAuthenticationTokenValue = loToken;
+
+            System.Net.NetworkCredential loCredentials = new System.Net.NetworkCredential();
+            loCredentials.Domain = txtDomain.Text;
+            loCredentials.UserName = txtUsername.Text;
+            loCredentials.Password = txtPassword.Text;
+            loService.Credentials = loCredentials;
+
+            loService.CrmAuthenticationTokenValue = loToken;
+            loService.Url = "http://" + txtServer.Text + ":" + txtPort.Text + "/mscrmservices/2007/crmservice.asmx";
+            return loService;
+        }
+
+        /// <summary>
+        /// Method to Get a Record and return
+        /// it in the form of a Dynamic Entity Object
+        /// to the calling function
+        /// </summary>
+        /// <param name="poGuid"></param>
+        /// <param name="psEntityName"></param>
+        /// <param name="psAttributeName"></param>
+        /// <param name="poService"></param>
+        /// <param name="paColumnSet"></param>
+        /// <returns></returns>
+        public static DynamicEntity GetDynamicEntityBasedOnGuid(Guid poGuid, string psEntityName, string psAttributeName, CrmService poService, ArrayList paColumnSet)
+        {
+            CrmService loService = poService;
+            QueryExpression loQuery = new QueryExpression();
+            DynamicEntity loDynamicEntity = null;
+            ColumnSet loColSet = new ColumnSet();
+
+            foreach (string lsColumnItem in paColumnSet)
+            {
+                loColSet.AddColumn(lsColumnItem);
+            }
+
+            try
+            {
+                ConditionExpression loCondition = new ConditionExpression(psAttributeName, ConditionOperator.Equal, poGuid);
+                FilterExpression loFilter = new FilterExpression();
+
+                loQuery.EntityName = psEntityName;
+                loQuery.ColumnSet = loColSet;
+                loFilter.Conditions.Add(loCondition);
+
+                loQuery.Criteria = loFilter;
+
+                RetrieveMultipleRequest loRetrieve = new RetrieveMultipleRequest();
+
+                loRetrieve.Query = loQuery;
+                loRetrieve.ReturnDynamicEntities = true;
+                RetrieveMultipleResponse loResponse = (RetrieveMultipleResponse)loService.Execute(loRetrieve);
+
+                if (loResponse.BusinessEntityCollection.BusinessEntities.Count > 0)
+                {
+                    loDynamicEntity = (DynamicEntity)loResponse.BusinessEntityCollection.BusinessEntities[0];
+                }
+            }
+            catch (System.Web.Services.Protocols.SoapException ex)
+            {
+                MessageBox.Show("Error: " + ex.Detail.InnerXml.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message.ToString());
+            }
+
+            return loDynamicEntity;
+        }
+
+        /// <summary>
+        /// Function to Check if Connection 
+        /// fields contain a valid value
+        /// </summary>
+        /// <param name="lsValidationError"></param>
+        /// <returns></returns>
+        public bool FieldsContainValue(ref string lsValidationError)
+        {
+            bool lbReturnValue = true;
+
+            if (txtServer.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Server Name";
+            }
+            else if (txtPort.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Server Port";
+            }
+            else if (txtOrganization.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Organization Name";
+            }
+            else if (txtUsername.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Username";
+            }
+            else if (txtPassword.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Password";
+            }
+            else if (txtDomain.Text == string.Empty)
+            {
+                lbReturnValue = false;
+                lsValidationError = "Please enter Domain Name";
+            }
+
+            return lbReturnValue;
+        }    
+        #endregion
+
+        #region Utility Methods
+        public string AppendToFileName(string psFileName, string psAppendText, string psSeparator)
+        {
+            string lsReturnValue = string.Empty;
+            string lsFileName = psFileName.Substring(0, psFileName.LastIndexOf("."));
+            string lsFileExtension = psFileName.Substring(psFileName.LastIndexOf(".") + 1);
+
+            lsReturnValue = lsFileName + psSeparator + psAppendText + "." + lsFileExtension;
+
+            return lsReturnValue;
+        }
+        #endregion
+
+        /// <summary>
+        /// Download Button Click Event
+        /// GUI Method to Connect to CRM 4.0 Webservice and 
+        /// download all the files specified from the database
+        /// saving them into the directory specified by the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string lsValidationError = string.Empty;
+            Int32 liDuplicateCounter = 1;
+
+            if (FieldsContainValue(ref lsValidationError) == true)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+
+                try
+                {
+                    string[] laAnnotationIds = txtGUID.Text.Split('\n');
+
+                    // Establish a Connection with CRM ---------------------------
+                    CrmService loService = GetCrmService();
+
+                    // Get Annotation ID -----------------------------------------
+                    // Guid loAnnotationId = new Guid(txtGUID.Text);
+
+                    // Define columns to retrieve from annotation record ---------
+                    ArrayList loColumnNames = new ArrayList();
+                    loColumnNames.Add("filename");
+                    loColumnNames.Add("documentbody");
+                    string lsErrorGUID = "An Error Occured Processing the following GUID: \r\n";
+                    bool lbErrorOccured = false;
+
+                    foreach (string lsAnnotationString in laAnnotationIds)
+                    {
+                        try
+                        {
+                            if (lsAnnotationString != string.Empty)
+                            {
+                                string lsAnnotationString2 = lsAnnotationString.Replace("\r", "");
+                                lsAnnotationString2 = lsAnnotationString2.Replace("{", "");
+                                lsAnnotationString2 = lsAnnotationString2.Replace("}", "");
+
+                                Guid loAnnotationId = new Guid(lsAnnotationString2);
+
+                                // Retrieve the annotation record ----------------------------
+                                DynamicEntity loDynamicEntity = GetDynamicEntityBasedOnGuid(loAnnotationId, "annotation", "annotationid", loService, loColumnNames);
+
+                                string lsNameOfFileToBeSaved = string.Empty;
+                                lsNameOfFileToBeSaved = loDynamicEntity.Properties["filename"].ToString();
+
+                                if (File.Exists(txtPath.Text + lsNameOfFileToBeSaved))
+                                {
+                                    // Rename File Name to avoid overwriting
+                                    lsNameOfFileToBeSaved = AppendToFileName(lsNameOfFileToBeSaved, liDuplicateCounter.ToString(), "_");
+                                    liDuplicateCounter++;
+                                }
+
+                                // Download the attachment in the current execution folder.
+                                using (FileStream fileStream = new FileStream(txtPath.Text + lsNameOfFileToBeSaved, FileMode.OpenOrCreate))
+                                {
+                                    //byte[] fileContent = new UTF8Encoding(true).GetBytes(loDynamicEntity.Properties["documentbody"].ToString());
+                                    byte[] fileContent = Convert.FromBase64String(loDynamicEntity.Properties["documentbody"].ToString());
+                                    fileStream.Write(fileContent, 0, fileContent.Length);
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            lsErrorGUID += lsAnnotationString + "\n";
+                            lbErrorOccured = true;
+                        }
+                    }
+
+                    if (lbErrorOccured == true)
+                    {
+                        txtError.Text = lsErrorGUID;
+                    }
+                    MessageBox.Show("Download Completed!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    Cursor.Current = Cursors.Default;
+                }
+            }
+            else
+            {
+                MessageBox.Show(lsValidationError);
+            }
+            
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath.ToString() + "\\";
+        }
+    }
+}
